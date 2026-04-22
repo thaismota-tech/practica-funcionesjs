d4d6f60c037d5f5a73719ce93784cb8dba45fe61
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-22 19:29:17 +0200

    He finalizado el ejercicio 1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Ejercicio 1: Saludo personalizado
+function saludar(nombre) {
+return `¡Hola, ${nombre}!`;
+}
+let resultado = saludar("Thaís");
+console.log(resultado);
+
+// Ejercicio 2: Suma de dos números
