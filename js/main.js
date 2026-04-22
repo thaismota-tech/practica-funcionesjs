683e2bd722f93dc467184fbb17d112d49cd1e0d8
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-22 19:54:03 +0200

    He finalizado el ejercicio 3

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ function suma(a, b) {
   return a + b;
 }
 console.log(suma(1989, 100));
+
+// Ejercicio 3: Números pares Escribe una función llamada esPar que reciba un número y devuelva true si el número es par o false si es impar.
+function esPar (num) {
+    return num % 2 === 0;
+}
+console.log(esPar(1));
+console.log(esPar(10));
+console.log(esPar(26));
+console.log(esPar(49));
+
+// Ejercicio 4: Números pares Escribe una función llamada esPar que reciba un número y devuelva true si el número es par o false si es impar.
